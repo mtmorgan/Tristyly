09e10a4293c14380c58f45ecb00c97130a93861d
Martin Morgan <martin.morgan@roswellpark.org>
2016-11-24 21:38:03 -0500

    expose graphical parameters

--- R/Plotting.R
+++ R/Plotting.R
@@ -9,7 +9,7 @@ NULL
 #' @examples
 #' plot_iterations(iterate(isoplethy(100), 1000))
 #' @export
-plot_iterations <- function(iterations, add=FALSE) {
+plot_iterations <- function(iterations, ..., type="b", add=FALSE) {
     long <- iterations[iterations$Morph == "Long", "Frequency"][[1]]
     mid <- iterations[iterations$Morph == "Mid", "Frequency"][[1]]
 
@@ -18,6 +18,6 @@ plot_iterations <- function(iterations, add=FALSE) {
     x_offset <- y / tan(pi/3)
     x <- x_offset + max * mid / sin(pi/3)
 
-    plot(y ~ x, type="b", xlim=c(0, 1), ylim=c(0, max), axes=FALSE)
+    plot(y ~ x, ..., type=type, xlim=c(0, 1), ylim=c(0, max), axes=FALSE)
     lines(c(0, 1, .5, 0), c(0, 0, max, 0))
 }
